05a4eebac11dd6ded9208d86f053383af0dcfede
Parallels <parallels@localhost.localdomain>
2023-02-07 08:59:33 -0800

    Add Q_inflow to argument in Write.f90

--- src/Write.f90
+++ src/Write.f90
@@ -1,4 +1,4 @@
-SUBROUTINE WRITE(time,nd,nr,ncell,ns,T_0,T_head,dbt,dpth,ice_thick,ice_temp,ICC)
+SUBROUTINE WRITE(time,nd,nr,ncell,ns,T_0,T_head,dbt,dpth,Q_inflow,ice_thick,ICC)
 !
 Implicit NONE
 !
@@ -11,5 +11,5 @@ real(8) :: time
 real    :: dbt
 !
 write (20,'(f12.4,4i6,4f8.2,f8.1,f8.2,2x,f8.1)')           &                                              
-            time,nd,nr,ncell,ns,T_0,T_head,dbt,dpth,ice_thick,ice_temp,ICC
+            time,nd,nr,ncell,ns,T_0,T_head,dbt,dpth,Q_inflow,ice_thick,ICC
 end SUBROUTINE WRITE
